440787477c9dc3f42c17c5954704b599bd3914f0
Roch Zaremba <84589346+RochZaremba@users.noreply.github.com>
2024-06-20 21:51:02 +0200

    Update time.hs

--- time.hs
+++ time.hs
@@ -7,8 +7,7 @@ import Control.Monad (unless)
 main :: IO ()
 main = do
     args <- getArgs
-    
-    -- Check if we have exactly two arguments
+
     unless (length args == 2) $ putStrLn "Usage: runhaskell time.hs <time1> <time2>" >> return ()
     
     let [arg1, arg2] = args
